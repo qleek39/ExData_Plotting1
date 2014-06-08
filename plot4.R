4c1adb196960db64560a29931d7812da2da1380a
Q L <casper.2006.02@gmail.com>
2014-06-09 00:49:09 +0100

    final commit

--- plot4.R
+++ plot4.R
@@ -3,5 +3,24 @@ twodays.power.cons.df = power.cons.df[power.cons.df$Date == "1/2/2007" | power.c
 twodays.power.cons.df$Date.Time.str <- paste(twodays.power.cons.df$Date, twodays.power.cons.df$Time)
 twodays.power.cons.df$Date.Time.posix <- strptime(twodays.power.cons.df$Date.Time.str, format = "%d/%m/%Y %H:%M:%S")
 
-png(file="./figure/plot3.png", width=1008, height=1008)
+png(file="./figure/plot4.png", width=1008, height=1008)
+par(mfcol=c(2, 2))
+plot(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Global_active_power, type ='n', ylab = "Global Active Power (killowats)")
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Global_active_power)
+
+
+plot(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Sub_metering_1, type = 'n', ylab = 'Energy Sub Metering')
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Sub_metering_1, col = 'gray')
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Sub_metering_2, col = 'red')
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Sub_metering_3, col = 'blue')
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = 20, col = c("gray", "red", "blue"))
+
+
+plot(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Voltage, type = "n", xlab = "datetime", ylab = "Voltage")
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Voltage)
+
+
+plot(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Global_reactive_power, type = "n", xlab = "datetime", ylab = "Global_reactive_power")
+lines(twodays.power.cons.df$Date.Time.posix, twodays.power.cons.df$Global_reactive_power)
+
 dev.off()
